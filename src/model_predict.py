c8abe4d11bc6ac92254d7649d2779b02440793be
Ben Dowdell <ben.dowdell87@gmail.com>
2020-07-06 19:32:50 -0500

    Adds interquartile range filter to input data

--- src/model_predict.py
+++ src/model_predict.py
@@ -6,6 +6,7 @@
 
 # Imports
 import pandas as pd
+import numpy as np
 import joblib
 import argparse
 import sys
@@ -30,8 +31,37 @@ input_file = args.input
 model_dir = args.directory
 output_dir = args.output
 
+
+def iqr_filter(data):
+    """
+    Takes a DataFrame containing well log data and filters outliers using Interquartile Range
+
+    Parameters:
+    data, pandas.DataFrame
+        The input data to be filtered.  Each column should be a well log curve
+
+    Returns:
+    df_filt, pandas.DataFrame
+        The filtered well curves
+    """
+    # create a copy of the original data
+    df_copy = data.copy()
+    # create a data frame containing the cleaned version of the data using IQR
+    df_filt = pd.DataFrame(index=df_copy.index)
+    # filter each column
+    for col in df_copy.columns.tolist():
+        q1 = df_copy[col].quantile(0.25)
+        q3 = df_copy[col].quantile(0.75)
+        IQR = q3 - q1
+        iqr_filt = (df_copy[col] >= q1 - 1.5 * IQR) & (df_copy[col] <= q3 + 1.5 * IQR)
+        df_filt[col] = df_copy[col].loc[iqr_filt]
+    return df_filt
+
+
 # Create a data frame to hold the input data
 df = pd.read_csv(input_file)
+df_iqr = iqr_filter(df.apply(np.log1p))
+df_iqr = df_iqr.apply(np.expm1).interpolate(limit_area='inside')
 
 # Get the contents of model_dir
 models = [f for f in os.listdir(model_dir) if '.joblib' in f]
@@ -63,7 +93,7 @@ knn_idx = [i for i, s in enumerate(models) if 'knn' in s]
 knn_model = joblib.load(model_dir + models[knn_idx[0]])
 
 # Prepare data for predictions
-X = df.drop(labels=['CAL', 'HRM', 'PE'], axis=1)
+X = df_iqr.drop(labels=['CAL', 'HRM', 'PE'], axis=1)
 
 # Make a prediction for each model
 print('Predicting y using Random Forest ...')
